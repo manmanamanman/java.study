362d156ef51a9311e9e8c382d8084692b62e0bff
qichangjun <2906635499@qq.com>
2026-03-18 22:07:05 +0800

    第六节二维数组-50%

--- java/src/bianliang/ZiMuZhuanHuan.java
+++ java/src/bianliang/ZiMuZhuanHuan.java
@@ -12,7 +12,7 @@ public class ZiMuZhuanHuan {
         //需注意Scanner接收中没有单独接收char类型的函数，需要使用next()接收字符串，然后使用charAt(0)，取字符串的第一位
         char zimu = input.next().charAt(0);
         //a的ASCII的值为97，A的ASCII的值为65，各对应字母之前相差32
-        int result = (int)zimu;
+        int result = zimu;
         if(result >= 97){
             zimu = (char)(result - 32);
         }else {
---
+++ java/src/erweisz/ErWeiShuZu.java
@@ -0,0 +1,9 @@
+package erweisz;
+
+public class ErWeiShuZu {
+    public static void main(String[] args) {
+        //二维数组
+        //二维数组定义格式：数据类型[][] 数组名=new 数据类型[行数][列数]
+        System.out.println();
+    }
+}
