75099a2e233cccd6f9201f97c6937b6c4cf196ba
qichangjun <2906635499@qq.com>
2026-03-30 11:32:15 +0800

    第11节多态-100%

--- java/src/leiheduixiang/duotai/DuoTai.java
+++ java/src/leiheduixiang/duotai/DuoTai.java
@@ -18,5 +18,11 @@ public class DuoTai {
         //作用：判断该对象是否属instanceof后面的类中，从而协助完成调用子类中特有的方法。配合多态使用
         //格式：对象 instanceof 类
 
+        //Random 随机数
+        //若需要使用随机数方法，需要先创建对象
+        //使用步骤：
+        // 1、创建对象   Random random=new Random();
+        // 2、调用需要使用类型的方法,如整数  random.nextInt(10)  --nextInt中的10代表0~9之间随机取值
+
     }
 }
